0a52c4e54d779572e26588273a891b745115d4c4
unknown <fernandezjoseluis2005@gmail.com>
2024-05-17 14:33:35 -0300

    Creación html

---
+++ Trabajo_Practico_3/assets/task.js
@@ -0,0 +1,7 @@
+let tareas = Array(prompt("Ingrese una tarea"));
+if (tareas!=undefined && tareas!=null){
+    function addTarea(tareas){
+        tareas.push(tareas);
+        console.log(Array);
+    }
+}
